82503338acaeaa4da961762d3106d094f88006dd
ajfx-01 <opeyemi.ajegbomogun@yahoo.com>
2025-11-12 15:18:02 +0100

    feat: added more images

--- lib/const.ts
+++ lib/const.ts
@@ -2,9 +2,9 @@ const rules: string[] = ["Check-in: 3 pm", "Pets: not allowed", "Check-out: 10 a
 
 const imageURLs = [
   "https://bookingenginecdn.hostaway.com/listing/23248-79029-SfTV9--CrlCv9---rP7uE5bSGCBJDCLTyaNLhH1NZ1B5k-68e64c0550cf3?width=1280&quality=70&format=webp&v=2",,
-  "https://bookingenginecdn.hostaway.com/listing/23248-79029-SfTV9--CrlCv9---rP7uE5bSGCBJDCLTyaNLhH1NZ1B5k-68e64c0550cf3?width=1280&quality=70&format=webp&v=2",
-  "https://bookingenginecdn.hostaway.com/listing/23248-79029-SfTV9--CrlCv9---rP7uE5bSGCBJDCLTyaNLhH1NZ1B5k-68e64c0550cf3?width=1280&quality=70&format=webp&v=2",
-  "https://bookingenginecdn.hostaway.com/listing/23248-79029-SfTV9--CrlCv9---rP7uE5bSGCBJDCLTyaNLhH1NZ1B5k-68e64c0550cf3?width=1280&quality=70&format=webp&v=2",
+  "https://bookingenginecdn.hostaway.com/listing/23248-79029-fywtCWHTiP-VXqNKyG4230oSaFqnTVdO1CHe6d3YqZ8-68e64c0aebc5a?width=1280&quality=70&format=webp&v=2",
+  "https://bookingenginecdn.hostaway.com/listing/23248-79029-Vnc7ic9YqxIzLBgNPT1rUb5Qru-C0bm2fD2FWgj5mIs-68e64c32ee29d?width=1280&quality=70&format=webp&v=2",
+  "https://bookingenginecdn.hostaway.com/listing/23248-79029-UvhHHGf--3miRxJJERsngMWZf9nHGJ4kiUousFFKKpmE-68e64c4237dc0?width=1280&quality=70&format=webp&v=2",
 ]
 
 const statusFilterItems: { [key: string]: string } = {
